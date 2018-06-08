a6a3f1f880c3aaaf945bd1ab0cb08d0ae7338c5a
Steven <le_steven@outlook.com>
2018-06-08 13:20:34 -0700

    add index and app

---
+++ src/common/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+  render() {
+    // mapped out client routes for reactRouter
+    return (
+      <Fragment>
+        'hello world!'
+      </Fragment>
+    )
+  }
+}
+
+
+
+
+// use withRouter to pass location to App
+export default App;
+
+
