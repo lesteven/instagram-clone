c9d90e84bd7d82a6f0b0af018d05e070e4699f3f
lesteven <le_steven@outlook.com>
2018-06-13 14:31:54 -0700

    refactor viewModule util tests

--- src/common/reduxModules/viewModule/utils/helperFunctions.unit.test.js
+++ src/common/reduxModules/viewModule/utils/helperFunctions.unit.test.js
@@ -8,12 +8,45 @@ const smallSize = 500;
 const bigSize = 1000;
 const none = 'none';
 const flex = 'flex';
+const block = 'block';
 
 
-test('showFlex w/ smallSize => none', () => {
-  expect(showFlex(smallSize)).toBe(none);
+describe('showFlex', () => {
+  test('w/ smallSize => none', () => {
+    expect(showFlex(smallSize)).toBe(none);
+  })
+
+  test('w/ bigSize => flex', () => {
+    expect(showFlex(bigSize)).toBe(flex);
+  })
 })
 
-test('showFlex w/ bigSize => flex', () => {
-  expect(showFlex(bigSize)).toBe(flex);
+describe('showBlock', () => {
+  test('w/ smallSize => none', () => {
+    expect(showBlock(smallSize)).toBe(none);
+  })
+
+  test('w/ bigSize => block', () => {
+    expect(showBlock(bigSize)).toBe(block);
+  })
 })
+
+describe('toggleFlex', () => {
+  test('w/ smallSize && display:none => flex', () => {
+    expect(toggleFlex(smallSize, none)).toBe(flex);
+  })
+
+  test('w/ smallSize && display:flex => none', () => {
+    expect(toggleFlex(smallSize, flex)).toBe(none);
+  })
+
+  test('w/ bigSize && display:flex => flex', () => {
+    expect(toggleFlex(bigSize, flex)).toBe(flex);
+  })
+
+  test('w/ bigSize && display:none => flex', () => {
+    expect(toggleFlex(bigSize, none)).toBe(flex);
+  })
+})
+
+
