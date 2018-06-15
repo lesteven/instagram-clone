9d100120ec2117b3587d0e6537ba5b3596ad98fc
lesteven <le_steven@outlook.com>
2018-06-15 12:10:42 -0700

    add all testing for view module

--- src/common/App.jsx
+++ src/common/App.jsx
@@ -2,9 +2,9 @@ import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import styles from './sharedViews/sharedCss.css';
 
+
 class App extends Component {
   render() {
-    // mapped out client routes for reactRouter
     return (
       <Fragment>
         'hello react page!'
@@ -18,5 +18,3 @@ class App extends Component {
 
 // use withRouter to pass location to App
 export default App;
-
-
