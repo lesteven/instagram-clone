f9047ef9c19015264656a84242c076afd3ff1480
lesteven <le_steven@outlook.com>
2018-06-25 16:20:38 -0700

    add policy agreement

--- src/common/views/home/components/SignUp.jsx
+++ src/common/views/home/components/SignUp.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import RegisterContainer from '../containers/RegisterContainer';
+import styles from './css/signUp.css'
 
+function PolicyAgreement() {
+  return (
+    <p> 
+      By signing up, you agree to our 
+      <strong> Terms</strong>, 
+      <strong> Data Policy</strong> and 
+      <strong> Cookies Policy</strong>. 
+    </p>
+  )
+}
 
 class SignUp extends Component {
   render() {
     return (
       <div className = 'signup-wrapper'>
         <h1 className = 'signup-logo'>Instagram</h1>
-        <div className = 'signup-box'>
-          <h4> Sign up to see photos and videos from your friends </h4>
-          <RegisterContainer />
-        </div>
+        <h4> Sign up to see photos and videos from your friends </h4>
+        <RegisterContainer />
+        <PolicyAgreement />
       </div>
     )
   }
