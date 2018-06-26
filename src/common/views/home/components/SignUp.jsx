68e425e846b7ad5d3932f237d3c0d0dba21b0aeb
lesteven <le_steven@outlook.com>
2018-06-25 17:39:51 -0700

    add sign-up components

--- src/common/views/home/components/SignUp.jsx
+++ src/common/views/home/components/SignUp.jsx
@@ -4,12 +4,33 @@ import styles from './css/signUp.css'
 
 function PolicyAgreement() {
   return (
-    <p> 
+    <p className='policy-agreement'> 
       By signing up, you agree to our 
       <strong> Terms</strong>, 
       <strong> Data Policy</strong> and 
       <strong> Cookies Policy</strong>. 
     </p>
+  )
+}
+
+function FacebookLogin() {
+  return (
+    <div className='fb-login'>
+      <button>
+        <div>f</div>
+        <a> Log in with Facebook </a>
+      </button>
+    </div>
+  )
+}
+
+function Divider() {
+  return (
+    <div className='divider'>
+      <span></span>
+      <p> OR </p>
+      <span></span>
+    </div>
   )
 }
 
@@ -19,6 +40,8 @@ class SignUp extends Component {
       <div className = 'signup-wrapper'>
         <h1 className = 'signup-logo'>Instagram</h1>
         <h4> Sign up to see photos and videos from your friends </h4>
+        <FacebookLogin />
+        <Divider />
         <RegisterContainer />
         <PolicyAgreement />
       </div>
