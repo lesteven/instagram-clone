ed0f8e5fa8f86a2695beb8aec01340076a34f863
lesteven <le_steven@outlook.com>
2018-07-30 17:07:52 -0700

    add other crud function tests

--- src/server/db/crudFunctions.js
+++ src/server/db/crudFunctions.js
@@ -7,23 +7,44 @@ import {
 
 export const find = (table, keys, data) => {
   const params = turnToParamsArr(keys, data);
-  const sql = `SELECT * FROM ${table} WHERE `
+  const valuesPH = valuesPlaceholder(keys); 
+
+  const findStr = `SELECT * FROM ${table}`;
+  const values = `WHERE ${keys} = ${valuesPH}`;
+
+  const sql = `${findStr} ${values}`
+  return query(sql, params);
 }
 
-export const update = (table, keys, data) => {
+export const update = (table, keys, data, newValue) => {
   const params = turnToParamsArr(keys, data);
   
+  const updateStr = `UPDATE ${table} SET ${keys} = ($1)`;
+  const values = `WHERE ${keys} = ($2)` 
+ 
+  const sql = `${updateStr} ${values}`;
+  return query(sql, [newValue, ...params]);
 }
 
 export const deleteData = (table, keys, data) => {
   const params = turnToParamsArr(keys, data);
-
+  const valuesPH = valuesPlaceholder(keys); 
+  
+  const updateStr= `DELETE FROM ${table}`;
+  const values =  `WHERE ${keys} = ${valuesPH}`;
+  
+  const sql = `${updateStr} ${values}`;
+  return query(sql, params);
 }
 
 export const insert = (table, keys, data) => {
   const params = turnToParamsArr(keys, data);
   const valuesPH = valuesPlaceholder(keys); 
-  const sql = `INSERT INTO ${table}(${keys}) VALUES${valuesPH} RETURNING *`;
+
+  const insertStr = `INSERT INTO ${table}(${keys})`;
+  const values = `VALUES${valuesPH} RETURNING *`
+
+  const sql = `${insertStr} ${values}`;
   return query(sql, params);
 }
 
--- src/server/db/tests/crudFunctions.unit.test.js
+++ src/server/db/tests/crudFunctions.unit.test.js
@@ -11,32 +11,51 @@ query.mockImplementation((sql, params) => {
   return [sql, params];
 })
 
-/*
+const key = 'email';
+const param = [email];
+
 describe('find', () => {
-  it('should', () => {
+  it('should select w/ one value', () => {
+    const sql = `SELECT * FROM ${table} WHERE ${key} = ($1)`;
+    expect(find(table, key, userData)).toEqual([sql, param])
   })
 })
 
 describe('update', () => {
-  it('should', () => {
+  it('should update w/ one value', () => {
+    const newEmail = 'ricflair@gmail.com';
+    const updateStr = `UPDATE ${table} SET ${key} = ($1)`;
+    const values = `WHERE ${key} = ($2)`;
 
+    const sql = `${updateStr} ${values}`;
+    const updateParams = [newEmail, ...param];
+    expect(update(table, key, userData, newEmail))
+      .toEqual([sql, updateParams])
   })
 
 })
 
 describe('delete', () => {
-  it('should', () => {
-
+  it('should delete w/ one value', () => {
+    const deleteStr = `DELETE FROM ${table}`;
+    const values = `WHERE ${key} = ($1)`;
+    
+    const sql = `${deleteStr} ${values}`;
+    expect(deleteData(table, key, userData)).toEqual([sql, param])
   })
 
 })
-*/
+
 describe('insert', () => {
-  it('should', () => {
-    const keys = 'email';
-    const params = [email];
-    const sql = `INSERT INTO ${table}(${keys}) VALUES($1) RETURNING *`;
+  it('should insert one value', () => {
+    const sql = `INSERT INTO ${table}(${key}) VALUES($1) RETURNING *`;
+    expect(insert(table, key, userData)).toEqual([sql, param]);
+  })
+  it('should insert multiple values', () => {
+    const keys = 'email,name,username,password';
+    const params = [email, name, username, password];
+    const place = `($1,$2,$3,$4)`;
+    const sql = `INSERT INTO ${table}(${keys}) VALUES${place} RETURNING *`;
     expect(insert(table, keys, userData)).toEqual([sql, params]);
   })
-
 })
