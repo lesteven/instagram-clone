0d4d127a4eb395795bad46fd183200db64af6713
lesteven <le_steven@outlook.com>
2018-08-10 16:37:09 -0700

    fix header section collapse

--- src/common/views/accounts/accountRoutes.js
+++ src/common/views/accounts/accountRoutes.js
@@ -1,6 +1,8 @@
 import makeRouteObject from '../../utils/makeRouteObject';
 import LoginPage from '../auth/components/LoginPage';
 import NotFound from '../generalComponents/NotFound';
+import EditAccounts from './components/EditAccounts';
+
 
 const exactTrue = true;
 
@@ -11,10 +13,13 @@ const main = '';
 const login = '/login';
 const loginTitle = 'Login';
 
+const edit = '/edit';
+const editTitle = 'Edit';
 
 const accountRoutes = [
   makeRouteObject(prefix, main, NotFound, exactTrue, main),
   makeRouteObject(prefix, login, LoginPage, exactTrue, loginTitle),
+  makeRouteObject(prefix, edit, EditAccounts, exactTrue, editTitle),
 ];
 
 export default accountRoutes;
---
+++ src/common/views/accounts/components/EditAccounts.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import MainTemplate from '../../mainTemplate/MainTemplate';
+
+
+class EditAccounts extends Component {
+  render() {
+    return (
+      <MainTemplate>
+      <p>edit accounts!</p>
+      </MainTemplate>
+    )
+  }
+}
+
+export default EditAccounts;
