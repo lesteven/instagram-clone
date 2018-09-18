cc45448ef904a692278fc0653c7b1a32f2a8fddb
lesteven <le_steven@outlook.com>
2018-09-17 18:16:46 -0700

    add triangle border

--- src/common/views/mainTemplate/navBar/components/SearchResults.jsx
+++ src/common/views/mainTemplate/navBar/components/SearchResults.jsx
@@ -6,18 +6,21 @@ import { Link } from 'react-router-dom';
 function SearchResults({ data }) {
   return (
     <div className = 'search-results'>
-      { data.map((e) => 
-        <Link to = {`/${e.username}`} 
-          key = { e.username + '/' + e.name }>
-        <div className = 'search-data'>
-          <img src = '/user.svg' />
-          <div className = 'search-userinfo'>
-            <p> { e.username } </p>
-            <p> { e.name } </p>
-          </div>
+      <div className = 'arrow'></div>
+        <div className = 'all-search-data'>
+          { data.map((e) => 
+            <Link to = {`/${e.username}`} 
+              key = { e.username + '/' + e.name }>
+            <div className = 'search-data'>
+              <img src = '/user.svg' />
+              <div className = 'search-userinfo'>
+                <p> { e.username } </p>
+                <p> { e.name } </p>
+              </div>
+            </div>
+            </Link>
+          )} 
         </div>
-        </Link>
-      )} 
     </div>
   )
 }
