49a48d6bb2f29d00cb8542b103c4cf3e14d238fa
lesteven <le_steven@outlook.com>
2018-11-09 10:26:47 -0800

    update about page

--- src/common/views/about/AboutPage.jsx
+++ src/common/views/about/AboutPage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import styles from './css/about.css';
 import MainTemplate from '../mainTemplate/MainTemplate';
+import data from './data';
+
 
 class AboutPage extends Component {
   render() {
@@ -8,8 +10,17 @@ class AboutPage extends Component {
     return (
       <MainTemplate>
         <div className = 'about'>
-        <h2> About </h2> 
-        <b>Steven Le</b>
+        <h2>About </h2> 
+        <p>
+          <b className='about-name'>Steven Le  </b>
+          (<a href= 'https://www.imstevenle.com'>portfolio</a>)
+        </p>
+        { data.map((each, index) => <p key = {index}>{ each }</p>) }
+          <p>
+            You can check out the code at <a href = 'https://github.com/lesteven/instagram-clone'>
+              github 
+            </a>
+          </p>
         </div>
       </MainTemplate>
     )
