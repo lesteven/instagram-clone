a0ac7f873e1c64b80b56aca62e5ba0a308f04458
lesteven <le_steven@outlook.com>
2018-12-03 13:34:24 -0800

    fix image feed size again

--- src/common/views/feed/components/Post.jsx
+++ src/common/views/feed/components/Post.jsx
@@ -9,17 +9,22 @@ class Post extends Component {
     const { userimage, username, imgname, created_at} = this.props.data;
     const { screenSize } = this.props;
     const style = {
-      height: screenSize * .65,
-    };
+      height: (screenSize < 550)? (screenSize * .9) : 550,
+    }
+    const imgStyle = {
+      maxHeight: (screenSize < 550)? (screenSize * .7) : 400,
+    }
     return (
-      <div className = 'post-wrapper'>
+      <div className = 'post-wrapper' style = { style } >
         <div className = 'post-user'>
           <img src = { userimage } />
           <Link to = { `/${username}` }>
             <p><strong>{ username }</strong></p>
           </Link>
         </div>
-        <img style = { style } src = { imgname } /> 
+        <div className = 'post-img-wrapper'>
+          <img src = { imgname } /> 
+        </div>
         <div className = 'post-data'>
           <p className = 'date'> 
             { new Date(created_at).toDateString() } 
